de2c31274f1c308a940e5f6ad685d929696e870a
Soeu <e2125045@cmaisonneuve.qc.ca>
2024-11-01 13:36:44 -0400

    s10c1 interraction page cours

--- js/cours.js
+++ js/cours.js
@@ -11,10 +11,31 @@ function toggleCours(btn) {
   btn.textContent = isVisible ? "+" : "-";
 }
 
-// Fonction pour afficher ou cacher les détails du cours de session choisie. 
+// Fonction pour afficher ou cacher les détails du cours de session choisie.
 document.querySelectorAll(".session").forEach((session) => {
   session.addEventListener("click", () => {
     // toggle Active class
-   session.classList.toggle("active");
+    session.classList.toggle("active");
   });
 });
+
+function myFunction() {
+  document.getElementById("myButton").onclick = function () {
+    var BordurCouleur = document.getElementsByClassName("cours-boxes");
+    var FondCouleur = document.getElementsByClassName("leCours");
+    var borderColor = getComputedStyle(document.documentElement)
+      .getPropertyValue("--couleur-bordur-video")
+      .trim();
+    var backgroundColor = getComputedStyle(document.documentElement)
+      .getPropertyValue("--couleur-video")
+      .trim();
+    for (var i = 0; i < BordurCouleur.length; i++) {
+      BordurCouleur[i].style.borderColor = borderColor;
+      FondCouleur[i].style.backgroundColor = backgroundColor;
+    }
+  };
+}
+
+// // Sélection des éléments dans le DOM
+// const cours = document.querySelectorAll(".cours");
+// const boiteCours = document.getElementsByClassName("cours-boxes");
