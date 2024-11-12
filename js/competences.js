652ce1bc2632174f56bc5e1ef12ba4b3ab5b0a75
Gauthier <e2290836@cmaisonneuve.qc.ca>
2024-11-12 16:47:56 -0500

    s11c2 carrousel compétence suite

--- js/competences.js
+++ js/competences.js
@@ -23,6 +23,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     for (const competence of competenceItems) {
       const itemsImages = competence.querySelectorAll(".item img");
+      // EN COURS
       const flechesGauche = competence.querySelectorAll(".flecheGauche");
       const flechesDroite = competence.querySelectorAll(".flecheDroite");
       for (let i = 0; i < itemsImages.length; i++) {
@@ -32,7 +33,13 @@ document.addEventListener("DOMContentLoaded", () => {
           itemsImages[i].parentElement.classList.toggle("inactif");
         }
       }
+      // EN COURS
+      // let afterStyle = document.createElement("style");
+      // afterStyle.innerHTML = ".inactif:after {content: '';position: absolute;background-color: rgba(0, 0, 0, 0.7);top: 0;left: 0;right: 0px;bottom: 0;}";
+      // document.head.appendChild(afterStyle);
+      // afterStyle.innerHTML = afterStyle.innerHTML.replace("rgba(0, 0, 0, 0.7);", "rgba(0, 0, 0, 0.2;");
     }
+
   } else {
     console.error(
       'Elements with class "button" or "competence-item" not found.'
