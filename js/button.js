5dd16a3c0ddaee5b531dd1dc5c1f6fe1ab8400e1
Zelara <119063805+Zelara@users.noreply.github.com>
2024-11-20 15:33:33 -0500

    L'animation du + marche bien dans l'accueil pour wordpress

--- js/button.js
+++ js/button.js
@@ -1,4 +1,12 @@
-function toggleSymbol() {
-  const button = document.querySelector(".top-right-button");
-  button.classList.toggle("clicked");
-}
+document.addEventListener("DOMContentLoaded", function () {
+  // Récupère tous les boutons avec la classe 'top-right-button'
+  const buttons = document.querySelectorAll(".top-right-button");
+
+  // Itère sur chaque bouton et ajoute un écouteur d'événement pour le clic
+  buttons.forEach((button) => {
+    button.addEventListener("click", function () {
+      // Ajoute ou enlève la classe 'clicked' au bouton cliqué
+      this.classList.toggle("clicked");
+    });
+  });
+});
