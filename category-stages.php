24a6b24b8b2849ce22f2240e913ddb1253d666c5
Zelara <119063805+Zelara@users.noreply.github.com>
2024-11-21 20:43:18 -0500

    Transformer les boutons dans la page de stage comme ceux dans la galerie

--- category-stages.php
+++ category-stages.php
@@ -1,32 +1,26 @@
 <?php
-// Ceci est l'équivalent du fichier carriere.htm
 // Appelle le fichier header.php
 get_header();
+
 ?>
 
-<!-- Main /////////////////////////////////////////////////////////////////////////////// -->
 <main class="carriere">
     <section class="intro-carr">
-        <h3>Stages et carrière</h3>
         <h3 class="section-stage">Stages et carrière</h3>
-
         <div class="trois-cat">
-            <a href="#stage">Stage</a>
-            <a href="#univer">Université</a>
-            <a href="#emploi">Marché d'Emploi</a>
+            <a href="#stage" class="animation-stage">Stage</a>
+            <a href="#univer" class="animation-universite">Université</a>
+            <a href="#emploi" class="animation-emploi">Marché d'Emploi</a>
         </div>
-
         <section class="intro-paragraphe">
             <img class="logo_carriere" src="<?php echo wp_get_attachment_url(303); ?>" alt="Diagram Venn TIM" />
-
             <div class="description-intro">
                 <p>Après ce beau programme de 3 ans en Technique d'intégration de multimédia, vous allez avoir deux
-                    choix de carrière totalement différents. Toutefois, il faut que vous compléter le stage obligatoire
+                    choix de carrière totalement différents. Toutefois, il faut que vous complétiez le stage obligatoire
                     à la fin de la session. Ensuite, vous pouvez choisir entre l'Université ou le Marché d'Emploi !
                 </p>
-                <p>LES TROIX SECTIONS SUIVANTES VOUS EXPLIQUERONT LES DÉTAILS DE CHAQUE CHOIX.</p>
-
-                <img class="logo_flecheCar" src="<?php echo wp_get_attachment_url(309); ?>" alt="Logo Direction Carrière">
+                <p>LES TROIS SECTIONS SUIVANTES VOUS EXPLIQUERONT LES DÉTAILS DE CHAQUE CHOIX.</p>
+                <img class="logo_flecheCar" src="<?php echo wp_get_attachment_url(309); ?>" alt="Logo Direction Carrière" />
             </div>
         </section>
     </section>
@@ -36,47 +30,49 @@ get_header();
             <div class="detail" id="stage">
                 <h5>Stage</h5>
                 <div class="contenu-image-description">
-                    <img src="<?php echo wp_get_attachment_url(305); ?>" alt="image qui illustre un stage">
+                    <div class="images-stage">
+                        <img src="<?php echo wp_get_attachment_url(472); ?>" alt="image qui illustre un stage" />
+                    </div>
                     <section class="paragraphe">
-                        <p>Une fois que vous atteinds le 5e session, vous allez preparer pour le stage
-                            de fin session. La plupart des préparations se font durant novembre jusqu'à janvier.
+                        <p>Une fois que vous atteignez la 5e session, vous allez vous préparer pour le stage
+                            de la fin de session. La plupart des préparations se font durant novembre jusqu'à janvier.
                             Il est important de bien se préparer pour ce stage, car il sert principalement à
                             développer vos compétences et à vous familiariser avec le marché d'emploi.
-                            Il est aussi préférable d'appliquer par vous même pour augmenter la chance de trouver un stage.
+                            Il est aussi préférable d'appliquer par vous-même pour augmenter la chance de trouver un stage.
                             Ce stage est obligatoire pour obtenir votre diplôme.
+                            <br><br>
+                            Pour savoir d'autres informations sur le stage ainsi que l'horaire de stage, consulter notre site officiel
+                            du collège : <a href="https://www.cmaisonneuve.qc.ca/">Collège de Maisonneuve</a>
                         </p>
-                        <p>Pour savoir d'autres informations sur le stage ainsi que l'horaire de stage, consulter notre site officiel
-                            du collège : <a href="https://www.cmaisonneuve.qc.ca/">Collège de Maisonneuve</a></p>
                     </section>
                 </div>
             </div>
-
             <div class="detail" id="univer">
                 <h5>Université</h5>
                 <div class="contenu-image-description">
-                    <img src="<?php echo wp_get_attachment_url(306); ?>" alt="image d'une Université">
+                    <img src="<?php echo wp_get_attachment_url(474); ?>" alt="image d'un Université" />
                     <section class="paragraphe">
-                        <p>Une fois que le stage est terminé, vous aurez aussi la chance de continuer votre
-                            étude à l'université pour obtenir un diplôme de votre choix.
-                        </p>
-                        <p>Le parcours universitaire permet aussi l'amélioration de certaines de vos compétences
+                        <p>Après avoir terminé votre stage, vous aurez également l'opportunité de continuer
+                            vos études à l'université pour obtenir un diplôme de votre choix. <br><br>
+                            Le parcours universitaire permet aussi l'amélioration certaine de vos compétences
                             acquises durant le programme de TIM.
                         </p>
                     </section>
                 </div>
             </div>
-
             <div class="detail" id="emploi">
                 <h5>Marché d'Emploi</h5>
                 <div class="contenu-image-description">
-                    <img src="<?php echo wp_get_attachment_url(307); ?>" alt="image emploi">
+                    <img src="<?php echo wp_get_attachment_url(470); ?>" alt="image emploi" />
                     <section class="paragraphe">
                         <p>Ce parcours est le meilleur choix si vous choisissez d'implémenter les compétences
-                            acquises directement dans le marché d'emploi.
+                            acquises directement dans le marché d'emploi. Il est le choix le plus rapide pour
+                            commencer une carrière de rêve ! <br> <br>Il s'agit également du parcours le plus difficile,
+                            car il faut que vous ayez les capacités de montrer vos compétences et les mettre en pratique dans un contexte professionnel.
+                            <br><br>
+                            Si vous êtes intéressé à commencer votre carrière au Collège de Maisonneuve, veuillez consulter
+                            notre site officiel pour plus d'informations.
                         </p>
-                        <p>Il est le choix le plus rapide pour commencer une carrière de rêve !</p>
-                        <p>Il est aussi le parcours le plus difficile, car il faut que vous soyez capable de
-                            démontrer vos compétences et de les appliquer dans un environnement professionnel.</p>
                     </section>
                 </div>
             </div>
@@ -89,8 +85,8 @@ get_header();
 ?>
 
 <!-- FAQ ////////////////////////////////////////////////////////////////////////////////// -->
-<?php echo do_shortcode('[faq]'); // Exécution du shortcode pour le FAQ 
+<?php echo do_shortcode('[faq]'); // Exécution du shortcode pour le faq 
 ?>
 
 <!-- Affiche le footer -->
-<?php get_footer(); ?>
+<?php get_footer() ?>
